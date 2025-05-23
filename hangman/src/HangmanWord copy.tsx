e180bd52b464b01ae9337f773f9689b83674deed
cooperbraun13 <cooperjbraun13@gmail.com>
2025-05-23 02:05:43 -0700

    done for the day

--- hangman/src/HangmanWord copy.tsx
+++ hangman/src/HangmanWord copy.tsx
@@ -1,3 +1,29 @@
 export function HangmanWord() {
-    
+  const word = "test"
+  const guessedLetters = ["t"]
+  return (
+    <div 
+      style={{ 
+        display: "flex", 
+        gap: "0.25em", 
+        fontSize: "6rem", 
+        fontWeight: "bold", 
+        textTransform: "uppercase", 
+        fontFamily: "monospace" 
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: "0.1em solid black"}}>
+          <span style={{
+            visibility: guessedLetters.includes(letter)
+              ? "visible"
+              : "hidden",
+          }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  )
 }
