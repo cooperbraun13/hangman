099f4ea48bf3d017288b080aa5c22c6bd168d10c
cooperbraun13 <cooperjbraun13@gmail.com>
2025-05-22 22:15:06 -0700

    setting up board

---
+++ hangman/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import words from "./wordList.json"
+import { Keyboard } from "./Keyboard"
+import { HangmanWord } from "./HangmanWord copy"
+import { HangmanDrawing } from "./HangmanDrawing"
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (
+    <div 
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
+}
+
+export default App
